ddbb4de39b8a12c397ab36ad326875b771ef6593
Home <937280517@qq.com>
2015-12-21 22:02:52 +0800

    Add ChildView at ArtistAlbumList

--- src/com/example/musicplayer/ArtistAlbumFragment.java
+++ src/com/example/musicplayer/ArtistAlbumFragment.java
@@ -1,10 +1,14 @@
 package com.example.musicplayer;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.AsyncQueryHandler;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
+import android.database.CursorWrapper;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -24,13 +28,15 @@ public class ArtistAlbumFragment extends Fragment {
 
 	private ExpandableListView mListView;
 	private ArtistAlbumListAdapter mAdapter;
-
+	private static Activity mActivity;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		if (DEBUG) {
 			Log.i(TAG, "onCreate");
 		}
 		super.onCreate(savedInstanceState);
+		mActivity = getActivity();
 	}
 
 	@Override
@@ -110,13 +116,16 @@ public class ArtistAlbumFragment extends Fragment {
 		private static final String TAG = "ArtistAlbumListAdapter";
 		private static final boolean DEBUG = true;
 
-        private int mGroupArtistIdIdx;
-        private int mGroupArtistIdx;
-        private int mGroupAlbumIdx;
-        private int mGroupSongIdx;
+		private int mGroupArtistIdIdx;
+		private int mGroupArtistIdx;
+		private int mGroupAlbumIdx;
+		private int mGroupSongIdx;
 
 		private ArtistAlbumFragment mFragment;
 		private QueryHandler mQueryHandler;
+		
+		private final String mUnknownAlbum;
+        private final BitmapDrawable mDefaultAlbumIcon;
 
 		static class ViewHolder {
 			TextView line1;
@@ -151,12 +160,37 @@ public class ArtistAlbumFragment extends Fragment {
 			super(context, cursor, groupLayout, groupFrom, groupTo,
 					childLayout, childFrom, childTo);
 			mFragment = fragment;
+			mUnknownAlbum = context.getString(R.string.unknown_album_name);
+			Resources r = mActivity.getResources();
+            mDefaultAlbumIcon = (BitmapDrawable) r.getDrawable(R.drawable.albumart_mp_unknown_list);
+			
 			mQueryHandler = new QueryHandler(context.getContentResolver());
 		}
 
 		@Override
-		protected Cursor getChildrenCursor(Cursor arg0) {
-			return null;
+		protected Cursor getChildrenCursor(Cursor groupCursor) {
+			long id = groupCursor.getLong(groupCursor.getColumnIndexOrThrow(MediaStore.Audio.Artists._ID));
+			String[] cols = new String[] { MediaStore.Audio.Albums._ID,
+					MediaStore.Audio.Albums.ALBUM,
+					MediaStore.Audio.Albums.NUMBER_OF_SONGS,
+					MediaStore.Audio.Albums.NUMBER_OF_SONGS_FOR_ARTIST,
+					MediaStore.Audio.Albums.ALBUM_ART };
+
+			Cursor c = MusicUtils.query(mActivity,
+					MediaStore.Audio.Artists.Albums.getContentUri("external",
+							id), cols, null, null,
+					MediaStore.Audio.Albums.DEFAULT_SORT_ORDER);
+
+			class MyCursorWrapper extends CursorWrapper {
+
+				public MyCursorWrapper(Cursor cursor) {
+					super(cursor);
+				}
+			}
+
+			MyCursorWrapper wrapper = new MyCursorWrapper(c);
+
+			return wrapper;
 		}
 
 		@Override
@@ -177,7 +211,18 @@ public class ArtistAlbumFragment extends Fragment {
 		@Override
 		public View newChildView(Context context, Cursor cursor,
 				boolean isLastChild, ViewGroup parent) {
-			return super.newChildView(context, cursor, isLastChild, parent);
+			View v = super.newChildView(context, cursor, isLastChild, parent);
+			ViewHolder vh = new ViewHolder();
+
+			vh.line1 = (TextView) v.findViewById(R.id.line1);
+			vh.line2 = (TextView) v.findViewById(R.id.line2);
+			vh.play_indicator = (ImageView) v.findViewById(R.id.play_indicator);
+			vh.icon = (ImageView) v.findViewById(R.id.icon);
+			// vh.icon.setBackgroundDrawable(background);
+			vh.icon.setPadding(0, 0, 1, 0);
+			v.setTag(vh);
+
+			return v;
 		}
 
 		@Override
@@ -186,36 +231,106 @@ public class ArtistAlbumFragment extends Fragment {
 			super.bindGroupView(view, context, cursor, isExpanded);
 			ViewHolder vh = (ViewHolder) view.getTag();
 
-            String artist = cursor.getString(mGroupArtistIdx);
-            String displayartist = artist;
-            boolean unknown = artist == null || artist.equals(MediaStore.UNKNOWN_STRING);
-            if (unknown) {
-                //displayartist = mUnknownArtist;
-            }
-            vh.line1.setText(displayartist);
+			String artist = cursor.getString(mGroupArtistIdx);
+			String displayartist = artist;
+			boolean unknown = artist == null
+					|| artist.equals(MediaStore.UNKNOWN_STRING);
+			if (unknown) {
+				// displayartist = mUnknownArtist;
+			}
+			vh.line1.setText(displayartist);
 
-            int numalbums = cursor.getInt(mGroupAlbumIdx);
-            int numsongs = cursor.getInt(mGroupSongIdx);
-            
-            String songs_albums = MusicUtils.makeAlbumsLabel(context,
-                    numalbums, numsongs, unknown);
-            
-            vh.line2.setText(songs_albums);
-            
-//            long currentartistid = MusicUtils.getCurrentArtistId();
-//            long artistid = cursor.getLong(mGroupArtistIdIdx);
-//            if (currentartistid == artistid && !isexpanded) {
-//                vh.play_indicator.setImageDrawable(mNowPlayingOverlay);
-//            } else {
-//                vh.play_indicator.setImageDrawable(null);
-//            }
+			int numalbums = cursor.getInt(mGroupAlbumIdx);
+			int numsongs = cursor.getInt(mGroupSongIdx);
+
+			String songs_albums = MusicUtils.makeAlbumsLabel(context,
+					numalbums, numsongs, unknown);
+
+			vh.line2.setText(songs_albums);
+
+			// long currentartistid = MusicUtils.getCurrentArtistId();
+			// long artistid = cursor.getLong(mGroupArtistIdIdx);
+			// if (currentartistid == artistid && !isexpanded) {
+			// vh.play_indicator.setImageDrawable(mNowPlayingOverlay);
+			// } else {
+			// vh.play_indicator.setImageDrawable(null);
+			// }
 		}
 
 		@Override
 		protected void bindChildView(View view, Context context, Cursor cursor,
 				boolean isLastChild) {
-			// TODO Auto-generated method stub
 			super.bindChildView(view, context, cursor, isLastChild);
+			ViewHolder vh = (ViewHolder) view.getTag();
+
+			String name = cursor.getString(cursor
+					.getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM));
+			String displayname = name;
+			boolean unknown = name == null
+					|| name.equals(MediaStore.UNKNOWN_STRING);
+			if (unknown) {
+				displayname = mUnknownAlbum;
+			}
+			vh.line1.setText(displayname);
+
+			int numsongs = cursor
+					.getInt(cursor
+							.getColumnIndexOrThrow(MediaStore.Audio.Albums.NUMBER_OF_SONGS));
+			int numartistsongs = cursor
+					.getInt(cursor
+							.getColumnIndexOrThrow(MediaStore.Audio.Albums.NUMBER_OF_SONGS_FOR_ARTIST));
+
+			// final StringBuilder builder = mBuffer;
+			// builder.delete(0, builder.length());
+			// if (unknown) {
+			// numsongs = numartistsongs;
+			// }
+
+			// if (numsongs == 1) {
+			// builder.append(context.getString(R.string.onesong));
+			// } else {
+			// if (numsongs == numartistsongs) {
+			// final Object[] args = mFormatArgs;
+			// args[0] = numsongs;
+			// builder.append(mResources.getQuantityString(
+			// R.plurals.Nsongs, numsongs, args));
+			// } else {
+			// final Object[] args = mFormatArgs3;
+			// args[0] = numsongs;
+			// args[1] = numartistsongs;
+			// args[2] = cursor
+			// .getString(cursor
+			// .getColumnIndexOrThrow(MediaStore.Audio.Artists.ARTIST));
+			// builder.append(mResources.getQuantityString(
+			// R.plurals.Nsongscomp, numsongs, args));
+			// }
+			// }
+			// vh.line2.setText(builder.toString());
+			//
+			 ImageView iv = vh.icon;
+			// // We don't actually need the path to the thumbnail file,
+			// // we just use it to see if there is album art or not
+			// String art = cursor.getString(cursor
+			// .getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM_ART));
+			// if (unknown || art == null || art.length() == 0) {
+			 iv.setBackgroundDrawable(mDefaultAlbumIcon);
+			// iv.setImageDrawable(null);
+			// } else {
+			// long artIndex = cursor.getLong(0);
+			// Drawable d = MusicUtils.getCachedArtwork(context, artIndex,
+			// mDefaultAlbumIcon);
+			// iv.setImageDrawable(d);
+			// }
+
+			// long currentalbumid = MusicUtils.getCurrentAlbumId();
+			// long aid = cursor.getLong(0);
+			// iv = vh.play_indicator;
+			// if (currentalbumid == aid) {
+			// iv.setImageDrawable(mNowPlayingOverlay);
+			// } else {
+			// iv.setImageDrawable(null);
+			// }
+
 		}
 
 		public AsyncQueryHandler getQueryHandler() {
@@ -224,10 +339,14 @@ public class ArtistAlbumFragment extends Fragment {
 
 		private void getColumnIndices(Cursor c) {
 			if (c != null) {
-                mGroupArtistIdIdx = c.getColumnIndexOrThrow(MediaStore.Audio.Artists._ID);
-                mGroupArtistIdx = c.getColumnIndexOrThrow(MediaStore.Audio.Artists.ARTIST);
-                mGroupAlbumIdx = c.getColumnIndexOrThrow(MediaStore.Audio.Artists.NUMBER_OF_ALBUMS);
-                mGroupSongIdx = c.getColumnIndexOrThrow(MediaStore.Audio.Artists.NUMBER_OF_TRACKS);
+				mGroupArtistIdIdx = c
+						.getColumnIndexOrThrow(MediaStore.Audio.Artists._ID);
+				mGroupArtistIdx = c
+						.getColumnIndexOrThrow(MediaStore.Audio.Artists.ARTIST);
+				mGroupAlbumIdx = c
+						.getColumnIndexOrThrow(MediaStore.Audio.Artists.NUMBER_OF_ALBUMS);
+				mGroupSongIdx = c
+						.getColumnIndexOrThrow(MediaStore.Audio.Artists.NUMBER_OF_TRACKS);
 			}
 		}
 
--- src/com/example/musicplayer/MusicUtils.java
+++ src/com/example/musicplayer/MusicUtils.java
@@ -3,8 +3,11 @@ package com.example.musicplayer;
 import java.util.Formatter;
 import java.util.Locale;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
 
 public class MusicUtils {
 	
@@ -41,5 +44,27 @@ public class MusicUtils {
 		}
 		return songs_albums.toString();
 	}
+	
+	public static Cursor query(Context context, Uri uri, String[] projection,
+            String selection, String[] selectionArgs, String sortOrder, int limit) {
+        try {
+            ContentResolver resolver = context.getContentResolver();
+            if (resolver == null) {
+                return null;
+            }
+            if (limit > 0) {
+                uri = uri.buildUpon().appendQueryParameter("limit", "" + limit).build();
+            }
+            return resolver.query(uri, projection, selection, selectionArgs, sortOrder);
+         } catch (UnsupportedOperationException ex) {
+            return null;
+        }
+        
+    }
+	
+	public static Cursor query(Context context, Uri uri, String[] projection,
+            String selection, String[] selectionArgs, String sortOrder) {
+        return query(context, uri, projection, selection, selectionArgs, sortOrder, 0);
+    }
 
 }
